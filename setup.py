fde24c5f13f6e97afe8ad399a26c9ebf34b2f655
luka-mladenovic <luka@kreativika.si>
2019-03-16 13:52:20 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='money',
       version='0.1',
       description='Implementation of Fowler\s Money',
-      url='',
+      url='https://github.com/luka-mladenovic/fowlers-money',
       author='Luka Mladenovic',
       author_email='',
       license='MIT',
