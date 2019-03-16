61c47ca6570a85c8b07da6bdfcb736f0b8d70467
luka-mladenovic <luka@kreativika.si>
2019-03-16 10:24:40 +0100

    Project setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='money',
+      version='0.1',
+      description='Implementation of Fowler\s Money',
+      url='',
+      author='Luka Mladenovic',
+      author_email='',
+      license='MIT',
+      packages=['money'],
+      install_requires=[
+          'pyyaml',
+      ],
+      zip_safe=False)
